da317cdd159f03f8b59ba68899e8d9e8443c115c
Fraser Tweedale <frase@frase.id.au>
2020-08-20 13:04:45 +1000

    2020: add tables

---
+++ src/Data/Tax/ATO/FY/FY2020.hs
@@ -0,0 +1,62 @@
+-- This file is part of hs-tax-ato
+-- Copyright (C) 2020  Fraser Tweedale
+--
+-- hs-tax-ato is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+{-# LANGUAGE DataKinds #-}
+
+-- | Tax tables for 2019–20 financial year.
+module Data.Tax.ATO.FY.FY2020 (tables) where
+
+import Control.Lens (review)
+
+import Data.Tax
+import Data.Tax.ATO.Common
+import Data.Tax.ATO.Days
+import qualified Data.Tax.ATO.FY.FY2019 as FY2019
+
+help :: (Fractional a, Ord a) => Tax (Money a) (Money a)
+help = thresholds'
+  [ (45881, 0.01)
+  , (52974, 0.01)
+  , (56152, 0.005)
+  , (59522, 0.005)
+  , (63093, 0.005)
+  , (66878, 0.005)
+  , (70891, 0.005)
+  , (75145, 0.005)
+  , (79653, 0.005)
+  , (84433, 0.005)
+  , (89499, 0.005)
+  , (94869, 0.005)
+  , (100561, 0.005)
+  , (106594, 0.005)
+  , (112990, 0.005)
+  , (119770, 0.005)
+  , (126956, 0.005)
+  , (134573, 0.005)
+  ]
+
+-- | From 1 July 2019, all study and training loans are covered by
+-- one set of thresholds and rates.  For backwards compatibility,
+-- 'ttHelp' and 'ttSfss' now refer to the same value.
+--
+tables :: (Ord a, Fractional a) => TaxTables 'CommonYear a
+tables = TaxTables
+  (ttIndividualIncomeTax FY2019.tables)
+  (medicareLevy (review money 22801))
+  medicareLevySurcharge
+  help
+  help
+  (lowIncomeTaxOffset <> lamito)
